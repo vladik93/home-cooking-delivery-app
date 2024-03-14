aa4cf463a09fd98ae5d66b14ff3caf8cc2383c42
Vladi <39379713+vladik93@users.noreply.github.com>
2024-03-15 00:23:54 +0200

    hero section
    status: work in progress

--- home-cooking-app/src/components/Hero.jsx
+++ home-cooking-app/src/components/Hero.jsx
@@ -3,6 +3,23 @@ import classes from './Hero.module.css';
 const Hero = () => {
   return (
   <section className={classes.hero}>
+    <div className={classes.titleWrapper}>
+      <h1>Unlock the Joy of Cooking: Fresh Ingredients, Inspired Recipes"</h1>
+      <h2>Fresh Ingredients, Easy Recipes: Delight in Home Cooking</h2>
+    </div>
+
+    <div className={classes.inputWrapper}>
+      <input type="text" />
+      <button>Get Started</button>
+    </div>
+
+    <div className={classes.textWrapper}>
+      <p>SUBSCRIBE NOW AND SAVE 30% ON YOUR FIRST ORDER</p>
+    </div>
+
+    <img src="images/hero.webp" className={classes.image} />
+    
+
     
   </section>
   
