655d65c595172d5e45418e6ac6f99d32d0268bd4
Vladi <39379713+vladik93@users.noreply.github.com>
2024-03-25 01:00:27 +0200

    update

---
+++ home-cooking-app/src/components/Slide.jsx
@@ -0,0 +1,17 @@
+import { styles } from "./Slide.module.css";
+import React from 'react';
+
+const Slide = () => {
+  return (
+    <div class={styles.slide}>
+
+
+
+
+
+
+    </div>
+  )
+};
+
+export default Slide;
